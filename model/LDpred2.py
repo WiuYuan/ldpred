a714fde87ebd6758ff33d89a44d620ee2fd0fd55
Yuan Wen <None>
2024-07-02 11:45:59 -0400

    add ldpred2-auto

--- model/LDpred2.py
+++ model/LDpred2.py
@@ -86,3 +86,103 @@ def ldpred2_grid(PM, sumstats, para):
         beta_gibbs = ldpred2_gibbs_one_sampling(PM[i], beta_hat, N, M, para)
         beta_gibbs_set.append(beta_gibbs * scale)
     return beta_gibbs_set
+
+
+def ldpred2_gibbs_auto(PM, beta_hat, N, M, para):
+    LD = PM["LD"]
+    # LD = inv(PM["precision"])
+    if isinstance(LD[0], np.float64):
+        LD = csr_matrix([[LD[0]]])
+    m = len(beta_hat)
+    h2 = para["h2"]
+    p = para["p"]
+    curr_beta = np.zeros(m)
+    avg_beta = np.zeros(m)
+    dotprods = np.zeros(m)
+    h2_per_var = h2 / (M * p)
+    inv_odd_p = (1 - p) / p
+    for k in range(-para["burn_in"], para["num_iter"]):
+        for j in range(m):
+            res_beta_hat_j = beta_hat[j] - (dotprods[j] - curr_beta[j])
+            # res_beta_hat_j = beta_hat[j] - dotprods[j]
+            C1 = h2_per_var * N[j]
+            C2 = 1 / (1 + 1 / C1)
+            C3 = C2 * res_beta_hat_j
+            C4 = C2 / N[j]
+            post_p_j = 1 / (1 + inv_odd_p * np.sqrt(1 + C1) * np.exp(-C3 * C3 / C4 / 2))
+            diff = -curr_beta[j]
+            # if post_p_j < p:
+            #     curr_beta[j] = 0
+            if post_p_j > np.random.rand():
+                curr_beta[j] = np.random.normal(C3, np.sqrt(C4))
+                diff += curr_beta[j]
+            else:
+                curr_beta[j] = 0
+            if k >= 0:
+                avg_beta[j] += C3 * post_p_j
+            if diff != 0:
+                dotprods += LD[:, j].toarray().flatten() * diff
+    return avg_beta / para["num_iter"]
+
+
+def ldpred2_auto(PM, sumstats, para):
+    p = np.random.rand()
+    h2 = np.random.rand()
+    curr_beta = []
+    avg_beta = []
+    dotprods = []
+    M = 0
+    for i in range(len(sumstats)):
+        m = len(sumstats[i]["beta"])
+        M += m
+        curr_beta.append(np.zeros(m))
+        avg_beta.append(np.zeros(m))
+        dotprods.append(np.zeros(m))
+
+    for k in range(-para["burn_in"], para["num_iter"]):
+        Mc = 0
+        # print(k, h2, p)
+        h2_per_var = h2 / (M * p)
+        inv_odd_p = (1 - p) / p
+        h2 = 0
+        for i in range(len(PM)):
+            if len(sumstats[i]["beta"]) == 0:
+                continue
+            N = np.array(sumstats[i]["N"]).astype(float)
+            beta_hat = np.array(sumstats[i]["beta"]).astype(float)
+            LD = PM[i]["LD"]
+            if isinstance(LD[0], np.float64):
+                LD = csr_matrix([[LD[0]]])
+            m = len(beta_hat)
+            for j in range(m):
+                res_beta_hat_j = beta_hat[j] - (dotprods[i][j] - curr_beta[i][j])
+                # res_beta_hat_j = beta_hat[j] - dotprods[j]
+                C1 = h2_per_var * N[j]
+                C2 = 1 / (1 + 1 / C1)
+                C3 = C2 * res_beta_hat_j
+                C4 = C2 / N[j]
+                post_p_j = 1 / (
+                    1 + inv_odd_p * np.sqrt(1 + C1) * np.exp(-C3 * C3 / C4 / 2)
+                )
+                diff = -curr_beta[i][j]
+                # if post_p_j < p:
+                #     curr_beta[j] = 0
+                if post_p_j > np.random.rand():
+                    curr_beta[i][j] = np.random.normal(C3, np.sqrt(C4))
+                    diff += curr_beta[i][j]
+                    Mc += 1
+                else:
+                    curr_beta[i][j] = 0
+                if k >= 0:
+                    avg_beta[i][j] += C3 * post_p_j
+                if diff != 0:
+                    dotprods[i] += LD[:, j].toarray().flatten() * diff
+            h2 += np.dot(curr_beta[i], LD.dot(curr_beta[i]))
+        p = np.random.beta(1 + Mc, 1 + M - Mc)
+    beta_auto_set = []
+    for i in range(len(sumstats)):
+        if len(sumstats[i]["beta"]) == 0:
+            beta_auto_set.append([])
+        else:
+            beta_auto_set.append(avg_beta[i] / para["num_iter"])
+    return beta_auto_set
